4c07d2adff59d16d8eddbcc7222bb0d005090a16
erick poleto <erick-poleto@hotmail.com>
2020-02-03 15:15:39 -0300

    aula 04 - concluida

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,20 +4,42 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
+
     adiciona(event){
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value.split('-').map((item, indice) => item - indice % 2)
-        );
-        console.log(data);
-        
-        let negociacao = new Negociacao(
+        /*instanciando objeto helper e criando variável data*/
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        this._listaNegociacoes.negociacoes.length = 0;
+
+        console.log(this._listaNegociacoes.negociacoes);
+
+    }
+
+
+    _criaNegociacao(){
+
+        let data = DateHelper.textoParaData(this._inputData.value);
+
+        return new Negociacao(
             data, 
             this._inputQuantidade.value,
             this._inputValor.value
+
         );
-        console.log(negociacao);
-    };
+
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
+    }
     
 }
--- client/js/atividades.js
+++ client/js/atividades.js
@@ -1,13 +1,44 @@
-/*trocando modo que data é exibida*/
+//trocando modo que data é exibida
 let dataString = ('10-05-2019');
 
 let data = new Date(dataString.split('-').reverse().join('/'));
 
 console.log(data);
 
-/*numeros dobrados se forem par*/
+//numeros dobrados se forem par
 let numeros = [3,2,11,20,8,7];
 
 let novosNumeros = numeros.map(item =>  item % 2 ? item * 2 : item);
 
 console.log(novosNumeros);
+
+//validar codigos
+
+class ValidaCodigos{
+    
+    static valida(codigo){
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(codigo)) throw new Error("deve estar no formato errado");
+   }   
+}
+
+//lista de nomes
+
+class Lista{
+
+    constructor(){
+        throw new Error('DateHelper cannot be instancied');
+    }
+
+    static mostraLista(...elementos){
+        let listaNomes = [].concat(elementos)
+        return console.log(listaNomes);
+    }
+}
+
+//ou 
+
+let listaDeNomes1 = ['Flávio', 'Rogers', 'Júlia'];
+let listaDeNomes2 = ['Vieira', 'Fernanda', 'Gerson'];
+let lista = listaDeNomes1.concat(listaDeNomes2);
+
+exibeNoConsole(lista);
